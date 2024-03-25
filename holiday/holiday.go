7cbfc20603c28a2d3b90975cc9600fc6af439821
ppdxzz <ppdxzz@outlook.com>
2024-03-25 13:53:42 +0800

    add:新增判断节假日以及双休的函数

---
+++ holiday/holiday.go
@@ -0,0 +1,88 @@
+package holiday
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+var holidayMap map[string]string
+
+type Holiday struct {
+	Name    string `json:"name"`
+	Date    string `json:"date"`
+	Type    int    `json:"type"`
+	Summary string `json:"summary"`
+}
+
+func init() {
+	holidayMap = make(map[string]string, 125)
+	var holidays []Holiday
+	file, err := os.Open("holiday2024.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	bytes, _ := reader.ReadBytes('\n')
+	err = json.Unmarshal(bytes, &holidays)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, holiday := range holidays {
+		holidayMap[holiday.Date] = strconv.Itoa(holiday.Type)
+	}
+}
+
+func checkDate(date string) error {
+	layout := "2006-01-02"
+	_, err := time.Parse(layout, date)
+	if err != nil {
+		return errors.New("日期格式不正确")
+	}
+	return nil
+}
+
+// IsHoliday 是否节假日
+func IsHoliday(date string) (bool, error) {
+	if err := checkDate(date); err != nil {
+		return false, err
+	}
+	value, exists := holidayMap[date]
+	if exists && value == "0" {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+// IsWeekend 是否双休
+func IsWeekend(date string) (bool, error) {
+	if err := checkDate(date); err != nil {
+		return false, err
+	}
+	value, exists := holidayMap[date]
+	if exists && value == "2" {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+// IsWeekday 是否工作日
+func IsWeekday(date string) (bool, error) {
+	if err := checkDate(date); err != nil {
+		return false, err
+	}
+	isHoliday, _ := IsHoliday(date)
+	isWeekend, _ := IsWeekend(date)
+	if !isHoliday && !isWeekend {
+		return true, nil
+	}
+	return false, nil
+}
